1f8d4518e01d1dfe48e9021210ffae1d7e4f166b
Matt Scott <matt@azorian.solutions>
2025-12-11 13:30:21 -0500

    Corrected example instantiation issue in the API models ACL module with newly added name field for roles.

--- src-api/models/api/acl/roles.py
+++ src-api/models/api/acl/roles.py
@@ -176,10 +176,10 @@ class RolesSchema(BaseApiModel):
         description='A list of roles found based on the current request criteria.',
         default_factory=list[RoleOutSchema],
         examples=[[
-            RoleOutSchema(slug='system_admin', description='A role for system administrators.'),
-            RoleOutSchema(slug='tenant_admin', description='A role for tenant administrators.'),
-            RoleOutSchema(slug='tenant_owner', description='A role for tenant owners.'),
-            RoleOutSchema(slug='zone_admin', description='A role for zone administrators.'),
+            RoleOutSchema(slug='system_admin', name='System Admin', description='A role for system administrators.'),
+            RoleOutSchema(slug='tenant_admin', name='Tenant Admin', description='A role for tenant administrators.'),
+            RoleOutSchema(slug='tenant_owner', name='Tenant Owner', description='A role for tenant owners.'),
+            RoleOutSchema(slug='zone_admin', name='Zone Admin', description='A role for zone administrators.'),
         ]],
     )
     """A list of roles found based on the current request criteria."""
